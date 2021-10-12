8aef06f3050712079e272191556e45293c6bc11c
Zara Gunner <20084586@mail.wit.ie>
2021-10-12 05:48:38 +0100

    initial project setup and css dependencies added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-red-600 - p6">
+    Hi 
+  </div>
 </template>
 
 <script>
